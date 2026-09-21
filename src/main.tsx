44d164c1b47c6a5461af37ea336f3481007267e9
Rudrank-cpu <rudranksuranje@gmail.com>
2026-09-21 19:54:58 +0530

    Standardize portal typography with Inter

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import '@fontsource/inter/latin-400.css';
+import '@fontsource/inter/latin-500.css';
+import '@fontsource/inter/latin-600.css';
+import '@fontsource/inter/latin-700.css';
 
 const savedTheme = localStorage.getItem('theme');
 
